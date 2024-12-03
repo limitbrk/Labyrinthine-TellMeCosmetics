dc1bd1d1cd2e94983a0965e76084ea446374c8f5
Chaiyatat Meema <limitbrk41@gmail.com>
2024-12-03 23:07:51 +0700

    Fix sometime UI crash when get item in first time

--- Mod.cs
+++ Mod.cs
@@ -6,6 +6,7 @@ using Il2CppCharacterCustomization;
 
 using TellMeCosmetics;
 using TellMeCosmetics.UI;
+using System;
 
 [assembly: MelonInfo(typeof(CustomizationPickupFinderMod), "Tell Me Cosmetics", "0.0.1", "LimitBRK")]
 [assembly: MelonGame("Valko Game Studios", "Labyrinthine")]
@@ -13,6 +14,7 @@ using TellMeCosmetics.UI;
 namespace TellMeCosmetics;
 public class CustomizationPickupFinderMod : MelonMod
 {
+    private ItemAlertUI alertui;
     public static CustomizationPickup CustomizationPickup { get; set; }
 
     public override void OnInitializeMelon()
@@ -22,39 +24,48 @@ public class CustomizationPickupFinderMod : MelonMod
 
     public override void OnSceneWasLoaded(int buildIndex, string sceneName)
     {
-        {
-            // Source of map name = Il2CppRandomGeneration.Contracts.MazeType
-            if (buildIndex >= 4 && sceneName.Contains("Maze"))
+        if (this.alertui == null) {
+            try {
+                RectTransform ui = GameObject.Find("Global/Global Canvas/Player_UI").GetComponent<RectTransform>();
+                if(ui != null) {
+                    LoggerInstance.Msg($"Initialization UI...");
+                    this.alertui = new ItemAlertUI(ui);
+                    LoggerInstance.Msg($"Created UI Successfully");
+                }
+            } 
+            catch (NullReferenceException)
             {
-                LoggerInstance.Msg($"Start Search in {sceneName}");
-                CustomizationPickup = null;
-                Thread t = new Thread(() =>
-                {
-                    int tried = 0;
-                    while (tried++ < 6)
-                    {
-                        CustomizationPickup = GameObject.FindObjectOfType<CustomizationPickup>();
-                        if (CustomizationPickup != null)
-                        {
-                            LoggerInstance.Msg($"Found Cosmetic ItemID: ({CustomizationPickup.ItemID}){CustomizationPickup.name}");
-
-
-                            // RndInventoryManager inventory = GameObject.Find("Global/Rnd Inventory Manager").GetComponent<RndInventoryManager>();
-                            // List<StoreCustomizationItemSO> items = inventory.GetItemsByIDs<StoreCustomizationItemSO>();
-
-                            RectTransform ui = GameObject.Find("Global/Global Canvas/Player_UI").GetComponent<RectTransform>();
-                            if (ui != null)
-                            {
-                                ItemAlertUI.CreateUI(ui,CustomizationPickup.name);
-                            }
-                            break;
-                        }
-                        LoggerInstance.Msg($"Not Found. Retry: {tried}");
-                        Thread.Sleep(5000);
-                    }
-                });
-                t.Start();
+                // SKIP if null
             }
+        }
+
+        // Source of map name = Il2CppRandomGeneration.Contracts.MazeType
+        if (this.alertui != null && buildIndex >= 4 && sceneName.Contains("Maze"))
+        {
+            LoggerInstance.Msg($"Start Search in {sceneName}");
+            CustomizationPickup = null;
+            Thread t = new Thread(() =>
+            {
+                int tried = 0;
+                while (tried++ < 6)
+                {
+                    CustomizationPickup = GameObject.FindObjectOfType<CustomizationPickup>();
+                    if (CustomizationPickup != null)
+                    {
+                        LoggerInstance.Msg($"Found Cosmetic ItemID: ({CustomizationPickup.ItemID}){CustomizationPickup.name}");
+                        this.alertui.ShowUIwithCooldown(CustomizationPickup.name);
+                        break;
+
+                        // This section try to get Item name/Item image
+                        // RndInventoryManager inventory = GameObject.Find("Global/Rnd Inventory Manager").GetComponent<RndInventoryManager>();
+                        // List<StoreCustomizationItemSO> items = inventory.GetItemsByIDs<StoreCustomizationItemSO>();
+                        
+                    }
+                    LoggerInstance.Msg($"Not Found. Retry: {tried}");
+                    Thread.Sleep(5000);
+                }
+            });
+            t.Start();
         }
     }
 }
--- ui/ItemAlertUI.cs
+++ ui/ItemAlertUI.cs
@@ -6,33 +6,44 @@ using UnityEngine.UI;
 namespace TellMeCosmetics.UI;
 public class ItemAlertUI
 {
-    public static void CreateUI(RectTransform tf, string itemname) 
+    GameObject ui_object;
+    public ItemAlertUI(RectTransform tf) 
     {
         // Create a new panel
-        GameObject ui = new GameObject("ItemAlert UI");
-        ui.transform.SetParent(tf);
+        this.ui_object = new GameObject("ItemAlert UI");
+        this.ui_object.SetActive(false);
+        this.ui_object.transform.SetParent(tf);
 
         Text myText;
-        myText = ui.AddComponent<Text>();
-        myText.text = itemname.Replace("(Clone)","");
+        myText = this.ui_object.AddComponent<Text>();
         myText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
         myText.fontSize = 24;
-        myText.color = new Color(1f, 1f, 1f, 0.75f);
+        // myText.color = new Color(1f, 1f, 1f, 0.75f);
         myText.alignment = TextAnchor.MiddleLeft;
         myText.horizontalOverflow = HorizontalWrapMode.Overflow;
 
-        RectTransform rectTransform = ui.GetComponent<RectTransform>();
+        RectTransform rectTransform = this.ui_object.GetComponent<RectTransform>();
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
         rectTransform.pivot = new Vector2(0, 0);
         rectTransform.anchoredPosition = new Vector2(10, 10);
+    }
 
-        MelonCoroutines.Start(SelfDestruct(ui));
+    public void ShowUIwithCooldown(string itemname) 
+    {
+        // Create a new panel
+        Text myText = this.ui_object.GetComponent<Text>();
+        if(myText == null){
+
+        }
+        myText.text = itemname.Replace("(Clone)","");
+        this.ui_object.SetActive(true);
+        MelonCoroutines.Start(SelfDestruct(this.ui_object));
     }
 
     private static IEnumerator SelfDestruct(GameObject ui)
     {
         yield return new WaitForSeconds(30f);
-        GameObject.Destroy(ui);
+        ui.SetActive(false);
     }
 }
